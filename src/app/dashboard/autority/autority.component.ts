eb176f7da55c8c57fec468adf3d4459670ed9818
EmileNapon <emilenapon0@gmail.com>
2025-02-18 21:52:20 +0000

    First commit

---
+++ src/app/dashboard/autority/autority.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autority',
+  imports: [],
+  templateUrl: './autority.component.html',
+  styleUrl: './autority.component.css'
+})
+export class AutorityComponent {
+
+}
